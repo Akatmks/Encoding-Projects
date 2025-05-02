23beec9d8c85385590bfef3f9e44f2f1824f5880
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-05-02 06:37:13 +0000

    Kekkan: Add --scd 0 for nothing

--- 2025/Kanpekiseijo WebRip/Kanpeki.fish
+++ 2025/Kanpekiseijo WebRip/Kanpeki.fish
@@ -56,7 +56,7 @@ function prepare
     end
     set scenes_file "$prefix/Kanpeki $episode.scenes.json"
     if not test -e $scenes_file
-        av1an -y --max-tries 5 --temp $temp_dir --verbose --log-level debug -i $source_file --sc-only --scenes $scenes_file --sc-method fast --extra-split 360 --min-scene-len 12 --zones $zones_file --encoder svt-av1 --video-params "--keyint -1 --input-depth 10 --tune 3 --qm-min 10 --chroma-qm-min 9 --enable-tf 1 --kf-tf-strength 1 --tf-strength 2 --sharpness 0 --film-grain 7 --psy-rd 2.8 --spy-rd 0 --color-primaries 1 --transfer-characteristics 1 --matrix-coefficients 1 --color-range 0"
+        av1an -y --max-tries 5 --temp $temp_dir --verbose --log-level debug -i $source_file --sc-only --scenes $scenes_file --sc-method fast --extra-split 360 --min-scene-len 12 --zones $zones_file --encoder svt-av1 --video-params "--scd 0 --keyint -1 --input-depth 10 --tune 3 --qm-min 10 --chroma-qm-min 9 --enable-tf 1 --kf-tf-strength 1 --tf-strength 2 --sharpness 0 --film-grain 7 --psy-rd 2.8 --spy-rd 0 --color-primaries 1 --transfer-characteristics 1 --matrix-coefficients 1 --color-range 0"
     end
     if not test -e $scenes_file
         set_color red ; echo "[prepare] Generated scenes file missing. Exiting..." ; set_color normal
