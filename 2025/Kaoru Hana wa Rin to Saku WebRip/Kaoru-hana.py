3d041decd1176169648b6cdf42883761d0bf236a
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-07-09 17:06:18 +0000

    [Kaoru-hana] starting

--- 2025/Kaoru Hana wa Rin to Saku WebRip/Kaoru-hana.py
+++ 2025/Kaoru Hana wa Rin to Saku WebRip/Kaoru-hana.py
@@ -13,8 +13,9 @@ from vstools import core, depth, DitherType, get_y, initialize_clip, join, SPath
 source_file = SPath(os.environ["SOURCE_FILE"])
 if not source_file.exists():
     raise FileNotFoundError("Source file not found.")
+source_lwi_file = SPath(os.environ["SOURCE_LWI_FILE"])
 
-src = core.lsmas.LWLibavSource(source_file)
+src = core.lsmas.LWLibavSource(source_file, cachefile=source_lwi_file)
 src = initialize_clip(src)
 
 
