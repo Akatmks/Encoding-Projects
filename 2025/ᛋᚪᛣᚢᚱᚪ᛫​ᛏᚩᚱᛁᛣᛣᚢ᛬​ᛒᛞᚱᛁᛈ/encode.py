41426611044c5e4b791d34f7c6e0e938914c6240
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-11-08 11:33:43 +0000

    [ᛏᚱ] 13.50 good enough

--- 2025/ᛋᚪᛣᚢᚱᚪ᛫​ᛏᚩᚱᛁᛣᛣᚢ᛬​ᛒᛞᚱᛁᛈ/encode.py
+++ 2025/ᛋᚪᛣᚢᚱᚪ᛫​ᛏᚩᚱᛁᛣᛣᚢ᛬​ᛒᛞᚱᛁᛈ/encode.py
@@ -29,5 +29,5 @@ def main(episode, final):
     output = SPath("Main") / f"{episode}.265"
 
     settings = settings_builder_x265(hist_scenecut="", frames=final.num_frames,
-                                     crf=13.33, qcomp=0.72, aq_strength=0.85)
+                                     crf=13.50, qcomp=0.72, aq_strength=0.85)
     return x265(settings, resumable=False, csv=SPath("Temp") / f"{episode}.x265_log.csv").encode(final, outfile=output)
