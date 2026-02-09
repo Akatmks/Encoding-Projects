335905234ad8eac6cfa5b90b9191e3e3f5e69003
Akatsumekusa <Akatsumekusa@protonmail.com>
2026-02-09 15:51:58 +0000

    [​ᛣᚾ] Update

--- 2026/ᛗᚪᚷᚢᛏᚢᛋᚳᛁ᛫​ᛣᚢᚾᚩᚾ᛫​ᚹᚪ᛫​ᛗᛁᛖᛏᛖᛁᚱᚢ᛬​⟨​ᚠ⟩​/sources.py
+++ 2026/ᛗᚪᚷᚢᛏᚢᛋᚳᛁ᛫​ᛣᚢᚾᚩᚾ᛫​ᚹᚪ᛫​ᛗᛁᛖᛏᛖᛁᚱᚢ᛬​⟨​ᚠ⟩​/sources.py
@@ -21,6 +21,7 @@ sources = {
     "04": Source(op=(3285, 5443)), # 2158!
     "05": Source(op=(1894, 4053)), # 2159!
     "06": Source(op=(864, 3021)),
+    "07": Source(op=(2422, 4579))
 }
 
 for episode in sources:
