b2e73aaa8ce8d7dfd69d5bab7a6939d505444169
Akatsumekusa <Akatsumekusa@protonmail.com>
2026-05-30 15:09:14 +0000

    [ᛋᛈ] Renew

---
+++ 2026/ᛋᚳᚪᚾᛈᛁᚾᛁᚩᚾ᛫​ᚾᚩ᛫​ᛗᚪᚷᚩ᛬​⟨​ᚢ⟩​/encode.py
@@ -0,0 +1,156 @@
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+
+import __main__
+
+from vsaa import based_aa
+from vsdenoise import DFTTest, frequency_merge
+from vskernels import Bilinear
+from vsmuxtools import settings_builder_x265, Setup, x265
+from vsscale import Rescale
+from vsTAAmbk import TAAmbk
+from vstools import core, finalize_clip, get_y, initialize_clip, insert_clip, join, replace_ranges, SPath, vs
+
+from sources import sources
+
+
+assert "EPISODE" in os.environ
+episode = os.environ["EPISODE"]
+assert episode in sources
+
+
+print(f"\033[1mSource:\033[0m \t{sources[episode].source_bd.name}", file=sys.stderr)
+src_bd = initialize_clip(core.bs.VideoSource(sources[episode].source_bd))
+
+if sources[episode].source_web:
+    print(f"\033[1mSource:\033[0m \t{sources[episode].source_web.name}", file=sys.stderr)
+    src_web = initialize_clip(core.bs.VideoSource(sources[episode].source_web))
+
+
+if sources[episode].source_web:
+    print(f"\t\tSource check in progress", file=sys.stderr, end="\r")
+    for fno, fr in enumerate(core.vszip.PlaneMinMax(core.akarin.Expr([src_bd, src_web], ["x y - abs", ""]), prop="Luma").frames()):
+        if fr.props["LumaMax"] > 72 << 8:
+            print(f"\033[1;31m\t\tSource check error on frame {fno}\033[0m", file=sys.stderr)
+    else:
+        print(f"\t\tSource check complete   ", file=sys.stderr)
+
+
+if sources[episode].source_web:
+    decheckerboard_web = src_web.dctf.DCTFilter(factors=[1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 1,
+                                                         1, 1, 1, 1, 1, 1, 1, 0.65])
+     
+    def high_minmax(clips, **_):
+        return core.akarin.Expr(clips, ["""
+    x 32768 >= y 32768 >= and
+      x y max
+      x 32768 <= y 32768 <= and
+        x y min
+        x ? ?
+    """, ""])
+    src = frequency_merge(src_bd, decheckerboard_web, lowpass=lambda clip, **kwargs: DFTTest().denoise(clip, **kwargs), mode_low=src_bd, mode_high=high_minmax, planes=[0])
+else:
+    src = src_bd
+
+
+if sources[episode].op_type == 1:
+    op_src = []
+    assert sources[episode].op[1] - sources[episode].op[0] == 2159
+    op_src.append(src_bd[sources[episode].op[0]:sources[episode].op[1]])
+    for op_ep in sources:
+        if op_ep != episode and sources[op_ep].op_type and sources[op_ep].op_type == sources[episode].op_type:
+            assert sources[op_ep].op[1] - sources[op_ep].op[0] == 2159
+            op_src.append(initialize_clip(core.bs.VideoSource(sources[op_ep].source_bd))[sources[op_ep].op[0]:sources[op_ep].op[1]])
+
+elif sources[episode].op_type == 3:
+    op_src = []
+    assert sources[episode].op[1] - sources[episode].op[0] >= 2157
+    op_src.append(src_bd[sources[episode].op[0]:sources[episode].op[0]+2157])
+    for op_ep in sources:
+        if op_ep != episode and sources[op_ep].op_type and sources[op_ep].op_type == sources[episode].op_type:
+            assert sources[op_ep].op[1] - sources[op_ep].op[0] >= 2157
+            op_src.append(initialize_clip(core.bs.VideoSource(sources[op_ep].source_bd))[sources[op_ep].op[0]:sources[op_ep].op[0]+2157])
+
+if sources[episode].op_type in [1, 3]:
+    print(f"\t\tfrequency_merge source check in progress", file=sys.stderr, end="\r")
+    for fno, fr in enumerate(core.vszip.PlaneMinMax(core.akarin.Expr([op_src[0], op_src[-1]], ["x y - abs", ""]), prop="Luma")[::49].frames()):
+        assert fr.props["LumaMax"] <= 64 << 8, f"{fno * 49}"
+    else:
+        print(f"\t\tfrequency_merge source check complete   ", file=sys.stderr)
+
+    op_merge = frequency_merge(*op_src, lowpass=lambda clip: DFTTest().denoise(clip))
+
+    src = insert_clip(src, op_merge, sources[episode].op[0])
+
+
+src_y = get_y(src)
+rs = Rescale(src_y, width=1500, height=843.75, kernel=Bilinear(), downscaler=Bilinear(linear=True))
+
+ds = rs.upscale
+re = rs.rescale
+
+ds_sub = core.akarin.Expr([ds, re], "x y -", format=vs.GRAYS)
+ds_soften = core.akarin.Expr([ds, re, ds_sub], """
+z 0 >
+    z[-1,-1] z[0,-1] z[1,-1] z[-1,0] z[0,0] z[1,0] z[-1,1] z[0,1] z[1,1]
+    sort9 least1! least2! drop7 least1@ 0 min least2@ 0 min + 0.5 * lim!
+    y x - 0.3 * lim@ max x +
+    x 0.8 * y 0.2 * + ?
+""")
+
+ds_noise = core.akarin.Expr([ds_soften, re, src_y], "z y - 0.8 * x +")
+ds_noise = join(ds_noise, src)
+
+aa_opp = TAAmbk(src_y, aatype=2, mclip=src.std.BlankClip(format=vs.GRAY16, color=65535))
+aa_opp = core.akarin.Expr([aa_opp, src_y], """
+x y - diff!
+diff@ 0 =
+    0
+    diff@ 0 >
+        diff@ 200 - 0 max
+        diff@ 200 + 0 min ? ?
+""", format=vs.GRAYS)
+aa_opp = core.akarin.Expr([aa_opp, src_y], """
+x 0 =
+    y
+    x 0 >
+    x[0,-1] 0 > x[0,1] 0 > x[-1,0] 0 > x[1,0] 0 >
+    or or or and
+    x 0 <
+    x[0,-1] 0 < x[0,1] 0 < x[-1,0] 0 < x[1,0] 0 <
+    or or or and or
+        y x +
+        y ? ?
+""", format=vs.GRAY16)
+aa_opp = join(aa_opp, src)
+
+aa_ed = based_aa(src, supersampler=False, mask_thr=80)
+
+lineart_final = ds_noise
+lineart_final = replace_ranges(lineart_final, aa_opp, sources[episode].op, exclusive=True)
+lineart_final = replace_ranges(lineart_final, src, (sources[episode].op[0] + 1737, sources[episode].op[0] + 2000), exclusive=True)
+lineart_final = replace_ranges(lineart_final, aa_ed, sources[episode].ed, exclusive=True)
+lineart_final = replace_ranges(lineart_final, src, sources[episode].text, exclusive=True)
+
+
+final = finalize_clip(lineart_final)
+
+
+if "__main__" in dir(__main__):
+    setup = Setup(episode, config_file=None, work_dir=SPath("Temp") / f"{episode}.vsmuxtools.tmp")
+    
+    output = SPath("Video") / f"{episode}.265"
+    assert final.num_frames <= 50000
+    grain = SPath("grain.bin")
+    settings = settings_builder_x265(asm="avx512", hist_scenecut="",
+                                     crf=14.20, cutree=True, aom_film_grain=grain)
+    x265(settings, resumable=False, csv=False).encode(final, outfile=output)
+else:
+    final.set_output()
---
+++ 2026/ᛋᚳᚪᚾᛈᛁᚾᛁᚩᚾ᛫​ᚾᚩ᛫​ᛗᚪᚷᚩ᛬​⟨​ᚢ⟩​/sources.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+import os
+from vstools import FrameRangeN, FrameRangesN, SPath
+from typing import Literal
+
+
+assert "VOL_01_BDMV_DIR" in os.environ
+assert "VOL_02_BDMV_DIR" in os.environ
+assert "WEB_DL_DIR" in os.environ
+
+vol_01_bdmv = SPath(os.environ["VOL_01_BDMV_DIR"])
+vol_02_bdmv = SPath(os.environ["VOL_02_BDMV_DIR"])
+web_dl = SPath(os.environ["WEB_DL_DIR"])
+
+
+@dataclass
+class Source:
+    source_bd: SPath
+    source_web: SPath | None = None
+    op: FrameRangeN | None = None
+    op_type: Literal[1, 2, 3, 4] | None = None # 1: Red, 2: Tan, 3: Teal, 4: Gold
+    ed: FrameRangeN | None = None
+    text: FrameRangesN | None = None
+
+
+sources = {
+    "01": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00002.m2ts",
+                 op=(4531, 6690), op_type=1, # Every episode 2159
+                 ed=(34813, 36971),
+                 text=[(6762, 6882), (36971, None)]),
+    "02": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00004.m2ts",
+                 op=(0, 2159), op_type=1,
+                 ed=(32200, 34357),
+                 text=[(2231, 2417), (34357, 34477), (34477, None)]),
+    "03": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00005.m2ts",
+                 op=(528, 2687), op_type=1,
+                 ed=(32488, 34647),
+                 text=[(2687, 2885), (34647, None)]),
+    "04": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00006.m2ts",
+                 op=(0, 2159), op_type=1,
+                 ed=(32488, 34645),
+                 text=[(3795, 3965), (34645, None)]),
+    "05": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00007.m2ts",
+                 op=(1152, 3309), op_type=2, # 2157
+                 ed=(32487, 34644),
+                 text=[(3597, 3801), (34644, None)]),
+    "06": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00008.m2ts",
+                 op=(1152, 3309), op_type=3, # 2157
+                 ed=(32488, 34645),
+                 text=[(3309, 3549), (34645, None)]),
+    "07": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00002.m2ts",
+                 op=(576, 2735), op_type=3,
+                 ed=(32489, 34646),
+                 text=[(2795, 2945), (34646, None)]),
+    "08": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00003.m2ts",
+                 op=(0, 2159), op_type=3,
+                 ed=(32489, 34646),
+                 text=[(3046, 3262), (34646, None)]),
+    "09": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00005.m2ts",
+                 op=(1703, 3861), op_type=3, # 2158
+                 ed=(32488, 34645),
+                 text=[(1068, 1248), (34645, None)]),
+    "10": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00006.m2ts",
+                 op=(744, 2903), op_type=3,
+                 ed=(32489, 34646),
+                 text=[(2903, 3083), (34646, None)]),
+    "11": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00007.m2ts",
+                 op=(0, 2159), op_type=3,
+                 ed=(32489, 34646),
+                 text=[(2159, 2358), (34646, None)]),
+    "12": Source(source_bd=vol_02_bdmv / "BDMV" / "STREAM" / "00008.m2ts",
+                 op=(0,  ), op_type=4,
+                 ed=(32247, None),
+                 text=[(2159, 2371)]),
+    "NCOP01": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00009.m2ts",
+                     op=(0, None)),
+    "NCOP02": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00010.m2ts",
+                     op=(0, None)),
+    "NCOP03": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00015.m2ts",
+                     op=(0, None)),
+    "NCOP04": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00016.m2ts",
+                     op=(0, None)),
+    "NCED01": Source(source_bd=vol_01_bdmv / "BDMV" / "STREAM" / "00017.m2ts",
+                     ed=(0, None)),
+}
+
+for episode in sources:
+    if matches := list(web_dl.glob(f"[S* - {episode} *")):
+        assert(len(matches) == 1)
+        sources[episode].source_web = matches[0]
